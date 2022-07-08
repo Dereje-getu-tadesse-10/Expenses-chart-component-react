ebbeb52ec782e9a68400c76630519cfdc5a3d9ce
Dereje <getutadessedereje@icloud.com>
2022-07-08 16:35:08 +0200

    Update: add data.json file

--- src/components/Container/Card/Card.jsx
+++ src/components/Container/Card/Card.jsx
@@ -1,87 +1,20 @@
 import styles from './Card.module.css'
 import { Footer } from '../../Footer/Footer'
-import 'chart.js/auto';
-import { Chart } from 'react-chartjs-2';
+import { SetChart } from '../Chart'
+import data from '../../data.json'
 
 
 export const Card = () => {
 
-    const states = {
-        labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-        datasets: [{
-            data: ['17.45', '35.91', '52.36', '31.07', '23.39', '43.28', '25.48'],
-            borderRadius:[5],
-            borderSkipped: false,
-            backgroundColor: [
-                  'hsl(10, 79%, 65%)',
-                  'hsl(10, 79%, 65%)',
-                  'hsl(186, 34%, 60%)',
-                  'hsl(10, 79%, 65%)',
-                  'hsl(10, 79%, 65%)',
-                  'hsl(10, 79%, 65%)',
-                  'hsl(10, 79%, 65%)'
-            ]
-        }]
-    }
-
-    const options = {
-        legend: {
-            display: false
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-    
-
+    const days = data.map(day => day.day)
+    console.log(days);
+    const amount = data.map(day => day.amount)
+    console.log(amount);
 
     return (
         <div className={styles.card}>
             <h1>Spending - Last 7 days</h1>
-            <Chart type='bar' data={states} 
-            options={{
-                plugins: {
-                    legend: {
-                        display: false
-                    },
-                    title: {
-                        display: false,
-                    },
-                    datalabels: {
-                        display: false
-                    }
-                },
-                yAxes: [{
-                    gridLines: {
-                        drawBorder: false,
-                    },
-                }],
-                scales:{
-                    x:{
-                        grid:{
-                            display:false,
-                            drawBorder:false,
-                            drawOnChartArea: false,
-                        }
-                    },
-                    y:{
-                        beginAtZero: true,
-                        ticks:{
-                            display:false
-                        },
-                        grid:{
-                            display: false,
-                            drawBorder:false,
-                            drawOnChartArea: true,
-                        }
-                    }
-                }
-            }}
-            />
+            <SetChart days={days} amount={amount}/>
             <Footer />
         </div>
     )
---
+++ src/components/Container/Chart.jsx
@@ -0,0 +1,72 @@
+import 'chart.js/auto';
+import { Chart } from 'react-chartjs-2';
+
+
+import React from 'react'
+
+export const SetChart = ({days, amount}) => {
+    console.log(days);
+
+    const data = {
+        labels: days,
+        datasets: [{
+            data: amount,
+            borderRadius:[5],
+            borderSkipped: false,
+            backgroundColor: [
+                  'hsl(10, 79%, 65%)',
+                  'hsl(10, 79%, 65%)',
+                  'hsl(186, 34%, 60%)',
+                  'hsl(10, 79%, 65%)',
+                  'hsl(10, 79%, 65%)',
+                  'hsl(10, 79%, 65%)',
+                  'hsl(10, 79%, 65%)'
+            ]
+        }]
+    }
+
+    const options = {
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: false,
+            },
+            datalabels: {
+                display: false
+            }
+        },
+        yAxes: [{
+            gridLines: {
+                drawBorder: false,
+            },
+        }],
+        scales:{
+            x:{
+                grid:{
+                    display:false,
+                    drawBorder:false,
+                    drawOnChartArea: false,
+                }
+            },
+            y:{
+                beginAtZero: true,
+                ticks:{
+                    display:false
+                },
+                grid:{
+                    display: false,
+                    drawBorder:false,
+                    drawOnChartArea: true,
+                }
+            }
+        }
+    }
+
+    return (
+        <Chart type='bar' data={data} options={options} />
+    )
+}
+
+
